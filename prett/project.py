671d58bfb012aa2907ad0b44a81f96ed4541e9c1
SF-Zhou <sfzhou.scut@gmail.com>
2017-09-23 18:03:57 +0800

    added setting file check when load

--- prett/project.py
+++ prett/project.py
@@ -7,14 +7,24 @@ import typing
 
 
 class SaveInterface(ValueModel):
-    def load(self):
-        if os.path.exists(self.filename):
+    def load(self, check_key: str, check_value: str):
+        if not os.path.exists(self.filename):
+            raise FileNotFoundError('Setting File [{}] Not Found'.format(self.filename))
+
+        try:
             with codecs.open(self.filename, 'r', encoding='utf-8') as f:
-                self.value = json.load(f)
+                obj = json.load(f)
+                assert isinstance(obj, dict)
+                assert obj.get(check_key, None) == check_value
+                self.value = obj
+        except AssertionError as e:
+            raise e
+        except ValueError as e:
+            raise e
 
     def save(self):
         with codecs.open(self.filename, 'w', encoding='utf-8') as f:
-            json.dump(self.value, f, indent=2)
+            json.dump(self.value, f, indent=2, ensure_ascii=False)
 
     @property
     def path(self) -> typing.Optional[str]:
