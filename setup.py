43ebc0bf953815295e3ba9e8c95981c1e067df15
SF-Zhou <sfzhou.scut@gmail.com>
2018-01-14 18:55:07 +0800

    bump version to 0.09

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prett',
-    version='0.0.8',
+    version='0.0.9',
     description='A Pretty Project Framework',
 
     url='https://github.com/sf-zhou/prett',
