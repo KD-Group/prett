4599b988a7c12eed8f5a2c16aa0b40c03fb539b6
Jefung <jefung865424525@gmail.com>
2019-08-19 20:40:30 +0800

    fix pwd error
    Change-Id: I2f8c2bfb290106a901bc9166816a724b3aea956e

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prett',
-    version='0.1.1',
+    version='0.1.2',
     description='A Pretty Project Framework',
 
     url='https://github.com/sf-zhou/prett',
